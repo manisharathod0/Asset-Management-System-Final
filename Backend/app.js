7b4465d93d5ad42f47bc1a736b1fd6f42af2167e
margi29 <margivadodaria@gmail.com>
2025-03-17 13:19:29 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -37,9 +37,6 @@ app.use((err, req, res, next) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-=======
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
