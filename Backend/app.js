3b6ef24f8436b9186e185fdc017b879fbbe8d885
Skrtd <138147720+Skrtd@users.noreply.github.com>
2025-03-23 12:53:58 +0530

    removed Reports

--- Backend/app.js
+++ Backend/app.js
@@ -37,7 +37,7 @@ const startServer = async () => {
     
     // Enable CORS
     app.use(cors({ 
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+      origin: ["http://localhost:5173", "http://127.0.0.1:5174"],
       credentials: true 
     }));
     
